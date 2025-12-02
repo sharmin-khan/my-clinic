34ff5bf7db1a04f8d439dfc5070ae6154086e98e
sharmin-khan <sharminkhancse@gmail.com>
2025-12-02 11:59:32 +0600

    Add active link functionality for navbar (desktop & mobile)

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const mobileMenu = document.getElementById("mobileMenu");
 menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
 });
+
+// Desktop Links
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.forEach(l => l.classList.remove("text-primary"));
+    link.classList.add("text-primary");
+  });
+});
+
+// Mobile Links
+const mobileLinks = document.querySelectorAll(".mobile-nav-link");
+mobileLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    mobileLinks.forEach(l => l.classList.remove("text-primary"));
+    link.classList.add("text-primary");
+    // Mobile menu close after click
+    document.getElementById("mobileMenu").classList.add("hidden");
+  });
+});
